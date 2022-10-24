97efd133d4ef7d7b082ff0728dfa798f677a37b6
Alek Jarmov <alek.jarmov@gmail.com>
2022-10-25 00:48:33 +0200

    Added filters

---
+++ src/main/java/mk/ukim/finki/wp/lab/web/filter/CourseFilter.java
@@ -0,0 +1,35 @@
+package mk.ukim.finki.wp.lab.web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter
+public class CourseFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        Long id =(Long) request.getSession().getAttribute("selectedCourse");
+        if(id==null){
+            response.sendRedirect("/SelectCourse");
+        }
+        else{
+            filterChain.doFilter(request,response);
+        }
+
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
