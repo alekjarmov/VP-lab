61c9e97ecd10c57a18fa4ef2669a97536463d57e
Alek Jarmov <alek.jarmov@gmail.com>
2022-10-25 01:27:35 +0200

    Fixed filters

--- src/main/java/mk/ukim/finki/wp/lab/web/filter/CourseFilter.java
+++ src/main/java/mk/ukim/finki/wp/lab/web/filter/CourseFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.*;
 
 @WebFilter
 public class CourseFilter implements Filter {
@@ -12,15 +13,33 @@ public class CourseFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        Long id =(Long) request.getSession().getAttribute("selectedCourse");
-        if(id==null){
-            response.sendRedirect("/SelectCourse");
-        }
-        else{
-            filterChain.doFilter(request,response);
+        String id = (String) request.getSession().getAttribute("selectedCourse");
+        String path = request.getServletPath();
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        List<String> goodPaths = new ArrayList<>();
+
+        Map<String, List<String>> excludedRoutes = new HashMap<>();
+        excludedRoutes.put("/listCourses", Arrays.asList("GET"));
+        excludedRoutes.put("/AddStudent", Arrays.asList("POST"));
+
+        String method = request.getMethod();
+        boolean doRedirect = shouldRedirect(excludedRoutes, path, method);
+        if (doRedirect && id == null) {
+            response.sendRedirect("/listCourses");
+        } else {
+            filterChain.doFilter(request, response);
         }
 
+    }
 
+    public boolean shouldRedirect(Map<String, List<String>> excludedRoutes, String path, String method) {
+        if (excludedRoutes.containsKey(path)) {
+            List<String> methods = excludedRoutes.get(path);
+            return !methods.contains(method);
+        }
+        return true;
     }
 
     @Override
