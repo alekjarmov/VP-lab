99fd41aa2357e59f6fab1e9c9ef9b4b8436d3f7a
Alek Jarmov <alek.jarmov@gmail.com>
2022-10-24 02:47:19 +0200

    Initial

---
+++ src/main/java/mk/ukim/finki/wp/lab/LabApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabApplication.class, args);
+    }
+
+}
