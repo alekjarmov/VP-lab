0c22a70a541c4d7a0abce675eb344a1a52b265cf
Alek Jarmov <alek.jarmov@gmail.com>
2022-12-28 17:48:39 +0100

    Lab 4 extra task

--- src/test/java/mk/ukim/finki/wp/lab/LabApplicationTests.java
+++ src/test/java/mk/ukim/finki/wp/lab/LabApplicationTests.java
@@ -2,6 +2,7 @@ package mk.ukim.finki.wp.lab;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -9,16 +10,24 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class LabApplicationTests {
     private MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext context;
+
     @BeforeEach
     public void setup(WebApplicationContext webApplicationContext) {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
@@ -33,6 +42,35 @@ class LabApplicationTests {
                 .andExpect(view().name("listCourses"))
                 .andExpect(model().attributeExists("courses"));
 
+    }
+
+    public void setupLogin() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity()) // enable security for the mock set up
+                .build();
+    }
+
+    @Test
+    void testSuccesfulLogin() throws Exception{
+        this.mockMvc.
+                perform(post("/login").param("username","admin").param("password","admin"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/courses"));
+    }
+    @Test
+    void testunsuccessfullogin() throws Exception {
+        this.mockMvc.
+                perform(MockMvcRequestBuilders.post("/login").param("username", "admin").param("password", "admin1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"));
+    }
+
+    @Test
+    void loginpage() throws Exception {
+        this.mockMvc.
+                perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(status().isOk());
     }
 
 }
